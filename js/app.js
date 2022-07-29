66f59007b53f8185b5630a4165978e6e5b068502
Abdul Basit <abdulbasitansari288@gmail.com>
2022-07-30 01:38:51 +0500

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+(function($, document, window){
+	
+	$(document).ready(function(){
+
+		// Cloning main navigation for mobile menu
+		$(".mobile-navigation").append($(".main-navigation .menu").clone());
+
+		// Mobile menu toggle 
+		$(".menu-toggle").click(function(){
+			$(".mobile-navigation").slideToggle();
+		});
+
+		var map = $(".map");
+		var latitude = map.data("latitude");
+		var longitude = map.data("longitude");
+		if( map.length ){
+			
+			map.gmap3({
+				map:{
+					options:{
+						center: [latitude,longitude],
+						zoom: 15,
+						scrollwheel: false
+					}
+				},
+				marker:{
+					latLng: [latitude,longitude],
+				}
+			});
+			
+		}
+	});
+
+	$(window).load(function(){
+
+	});
+
+})(jQuery, document, window);
